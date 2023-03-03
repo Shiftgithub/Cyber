5ea9db277260bc1b96d4dde59c9a59bc5a18d6f9
shiftgithub <mhturanmohammad@gmail.com>
2023-03-04 01:48:04 +0600

    updated

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('https://cyber-6y9f.onrender.com/', {
+  const response = await fetch('https://cyber-rwie.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
